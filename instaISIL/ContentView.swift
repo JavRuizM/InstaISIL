3492ee236888b53a8855b50242d06f296daa4c46
Jruiz <server.jarm.shalom@gmail.com>
2020-12-02 23:15:14 -0800

    Home

--- instaISIL/ContentView.swift
+++ instaISIL/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
     var body: some View {
@@ -17,8 +18,12 @@ struct ContentView: View {
                                                    Color("primary"),
                                                    Color("primary"),
                                                    Color("primary"),
-                                                   Color ("primary"),
-                                                   Color(.black)]
+                                                   Color("primary"),
+                                                   Color("primary"),
+                                                   Color("primary"),
+                                                   Color("primary"),
+                                                   Color("primary")
+            ]
             ),
             startPoint: .top,
             endPoint: .bottom).edgesIgnoringSafeArea(.all)
@@ -37,6 +42,8 @@ struct ContentView_Previews: PreviewProvider {
 struct Home: View{
     
     @State var index = 0
+    @State var status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+    
     var body: some View{
         
         VStack{
@@ -145,73 +152,120 @@ struct Login: View {
     
     var body: some View{
         
-        VStack{
+        ZStack{
             
             VStack{
                 
-                HStack(spacing: 15){
+                VStack{
                     
-                    Image(systemName: "envelope")
-                        .foregroundColor(.black)
-                    
-                    TextField("Ingresar usuario", text: self.$mail)
-                }
-                .padding(.vertical, 20)
-                
-                Divider()
-                
-                
-                HStack(spacing: 15) {
-                    
-                    Image(systemName: "lock")
-                        .resizable()
-                        .frame(width: 15, height: 18)
-                        .foregroundColor(.black)
-                    
-                    if self.visible{
-                        TextField("Ingresar contraseña", text: self.$pass)
-                    }
-                    else{
-                        SecureField("Ingresar contraseña", text: self.$pass)
-                    }
-                    
-                    Button(action:{
-                        self.visible.toggle()
-                    }){
-                        Image(systemName: "eye")
+                    HStack(spacing: 15){
+                        
+                        Image(systemName: "envelope")
                             .foregroundColor(.black)
+                        
+                        TextField("Ingresar usuario", text: self.$mail)
                     }
+                    .padding(.vertical, 20)
+                    
+                    Divider()
+                    
+                    
+                    HStack(spacing: 15) {
+                        
+                        Image(systemName: "lock")
+                            .resizable()
+                            .frame(width: 15, height: 18)
+                            .foregroundColor(.black)
+                        
+                        if self.visible{
+                            TextField("Ingresar contraseña", text: self.$pass)
+                        }
+                        else{
+                            SecureField("Ingresar contraseña", text: self.$pass)
+                        }
+                        
+                        Button(action:{
+                            self.visible.toggle()
+                        }){
+                            Image(systemName: "eye")
+                                .foregroundColor(.black)
+                        }
+                        
+                    }
+                    .padding(.vertical, 20)
                     
                 }
-                .padding(.vertical, 20)
+                .padding(.vertical)
+                .padding(.horizontal, 20)
+                .padding(.bottom, 40)
+                .background(Color.white)
+                .cornerRadius(10)
+                .padding(.top, 25)
                 
+                Button(action: {
+                    self.verify()
+                }){
+                    
+                    Text("LOGIN")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 100)
+                    
+                }
+                .background(LinearGradient(gradient: .init(colors:[Color(.white),
+                                                                   Color("primary"),
+                                                                   Color("primary"),
+                                                                   Color("primary")
+                                                                    ]),
+                                        startPoint: .leading,
+                                        endPoint: .trailing)
+                )
+                .cornerRadius(8)
+                .offset(y: -40)
+                .padding(.bottom, -40)
+                .shadow(radius: 15)
             }
-            .padding(.vertical)
-            .padding(.horizontal, 20)
-            .padding(.bottom, 40)
-            .background(Color.white)
-            .cornerRadius(10)
-            .padding(.top, 25)
             
-            Button(action: {}){
+            if self.alert{
                 
-                Text("LOGIN")
-                    .foregroundColor(.white)
-                    .fontWeight(.bold)
-                    .padding(.vertical)
-                    .frame(width: UIScreen.main.bounds.width - 100)
-                
+                ErrorView(alert: self.$alert, error: self.$error)
             }
-            .background(LinearGradient(gradient: .init(colors:[Color("primary"),
-                                                               Color("primary"),
-                                                               Color(.white)]
-            ),
-            startPoint: .leading,
-            endPoint: .trailing))
-            .cornerRadius(8)
-            .offset(y: -40)
-            .padding(.bottom, -40)
-            .shadow(radius: 15)
+        }
+        .padding(.vertical, 25)
+    }
+    
+    func verify(){
+        
+        if self.mail == "" && self.pass == ""
+        {
+            self.error = "Por favor llene las credenciales solicitadas"
+            self.alert.toggle()
+        }
+        else if self.mail == ""
+        {
+            self.error = "Debe ingresar credencial de usuario."
+            self.alert.toggle()
+        }
+        else if self.pass == ""
+        {
+            self.error = "Debe ingresar credencial para contraseña."
+            self.alert.toggle()
+        }
+        else
+        {
+            Auth.auth().signIn(withEmail: self.mail, password: self.pass){
+                (res, err) in
+                
+                if err != nil {
+                    self.error = err!.localizedDescription
+                    self.alert.toggle()
+                }
+                
+                print("success")
+                UserDefaults.standard.set(true, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+            }
         }
     }
 }
@@ -223,9 +277,13 @@ struct SignUp: View {
     @State var repass = ""
     @State var visiblePass = false
     @State var visibleRePass = false
+    @State var alert = false
+    @State var error = ""
     
     var body: some View{
         
+        ZStack{
+            
         VStack{
             
             VStack{
@@ -302,7 +360,9 @@ struct SignUp: View {
             .cornerRadius(10)
             .padding(.top, 25)
             
-            Button(action: {}){
+            Button(action: {
+                    self.verify()
+            }){
                 
                 Text("SIGNUP")
                     .foregroundColor(.white)
@@ -321,6 +381,39 @@ struct SignUp: View {
             .offset(y: -40)
             .padding(.bottom, -40)
             .shadow(radius: 15)
+        }
+            if self.alert{
+                
+                ErrorView(alert: self.$alert, error: self.$error)
+            }
+        }
+    }
+    
+    func verify(){
+        
+        if self.mail == "" && self.pass == "" && self.repass == ""
+        {
+            self.error = "Por favor llene las credenciales solicitadas."
+            self.alert.toggle()
+        }
+        else if self.mail == ""
+        {
+            self.error = "Debe ingresar credencial de usuario."
+            self.alert.toggle()
+        }
+        else if self.pass == ""
+        {
+            self.error = "Debe ingresar credencial para contraseña."
+            self.alert.toggle()
+        }
+        else if self.repass == ""
+        {
+            self.error = "Por favor confirme su contraseña."
+            self.alert.toggle()
+        }
+        else
+        {
+            
         }
     }
 }
@@ -341,6 +434,7 @@ struct ErrorView: View {
                     
                     Text("Error")
                         .font(.title)
+                        .fontWeight(.bold)
                         .foregroundColor(self.color)
                     
                     Spacer()
@@ -353,21 +447,23 @@ struct ErrorView: View {
                     .padding(.horizontal, 25)
                 
                 Button(action:{
-                    
+                    self.alert.toggle()
                 }){
                     Text("Cancel")
                         .foregroundColor(.white)
                         .padding(.vertical)
                         .frame(width: UIScreen.main.bounds.width-120)
                 }
-                .background(Color("Color"))
+                .background(Color("primary"))
                 .cornerRadius(10)
                 .padding(.top, 25)
             }
-            .frame(width: UIScreen.main.bounds.width-70)
+            .padding(.vertical, 25)
+            .frame(width: UIScreen.main.bounds.width-32)
             .background(Color.white)
             .cornerRadius(15)
         }
-        .background(Color.black.opacity(0.35).edgesIgnoringSafeArea(.all))
+        .background(Color.black.opacity(0.25).edgesIgnoringSafeArea(.all))
+        .cornerRadius(15)
     }
 }
---
+++ instaISIL/IPhone/Classes/Home/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  instaISIL
+//
+//  Created by user179028 on 12/2/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController{
+    
+}
